5f03fc39d265b2ac9e67e074b07574fde19a8b2f
wuwu0102 <wuwu0102@users.noreply.github.com>
2026-04-25 12:38:55 +0000

    deploy: ca4bbbf38a9008787583cd9d6f04dd60a5a87f8b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,8 +25,8 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "8988e4c8297e6dfcc1759bdb7de31746",
-"main.dart.js": "2d327c2da7e7b4c7a0c4fe10a0f17c7f",
+"flutter_bootstrap.js": "c5eb7310e7ff9f12e417de51c1285d3a",
+"main.dart.js": "68c6fb48088c2f0020969920e03c55fc",
 "version.json": "117dccb2ea340fe5c3213b41fb0ccf2a"};
 // The application shell files that are downloaded before a service worker can
 // start.
