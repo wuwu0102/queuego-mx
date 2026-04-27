dd10e6862eb23ce08d14bf5aeafbe5252d2c5a2b
wuwu0102 <wuwu0102@users.noreply.github.com>
2026-04-27 06:02:30 +0000

    deploy: 2121b1749911ecf410a78faa0b8dd22ec1752796

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,13 +7,13 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"assets/fonts/MaterialIcons-Regular.otf": "f3b2ae8a3e780c0ed8b410db317f2ffe",
+"assets/fonts/MaterialIcons-Regular.otf": "1d396afc4c5377beef57f30d38af6f9e",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
-"assets/NOTICES": "b4ad106f7ffbfcc7637b1baf68cb16fe",
+"assets/NOTICES": "a60fe4377f24a0daf14f68c4ceb7fabf",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "manifest.json": "d412b500e5ac6577421886494a6a9ec3",
-"index.html": "ed08b62bd832f955547d869d521b8445",
-"/": "ed08b62bd832f955547d869d521b8445",
+"index.html": "1868b4461d0970b1333a727730cb0132",
+"/": "1868b4461d0970b1333a727730cb0132",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/chromium/canvaskit.js.symbols": "a012ed99ccba193cf96bb2643003f6fc",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
@@ -25,8 +25,8 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "5c70070fde15b0f241b7a2442bc1ccd9",
-"main.dart.js": "88f6804c0cf07b3c369fe37d0c8b24a1",
+"flutter_bootstrap.js": "4fd2f641e8f56b7530040b2590cbc59e",
+"main.dart.js": "6cd73a998d56452fae3e8a0910739c3b",
 "version.json": "117dccb2ea340fe5c3213b41fb0ccf2a"};
 // The application shell files that are downloaded before a service worker can
 // start.
